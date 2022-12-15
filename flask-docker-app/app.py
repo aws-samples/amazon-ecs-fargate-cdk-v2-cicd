ab98226093a5c0b23930d8b25bd1667b176bc7df
Emile Baizel <baizele@amazon.com>
2022-12-15 12:26:02 -0500

    update css for site

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -9,7 +9,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = flask.request.args.get("name", "AnyCompany")
+    name = flask.request.args.get("name", "Flask-demo")
     time = datetime.datetime.now()
     python_version = platform.python_version()
     aws_platform = os.environ.get('PLATFORM', 'Amazon Web Services')
